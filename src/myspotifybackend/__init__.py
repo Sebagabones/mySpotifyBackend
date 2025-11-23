13669e89f26045e14e0c6fe20b183153010ac084
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-11-23 16:20:19 +0800

    nix build actually runs correctly now

--- src/myspotifybackend/__init__.py
+++ src/myspotifybackend/__init__.py
@@ -2,14 +2,14 @@ import os
 
 import uvicorn
 
-from myspotifybackend.main import Application
+from myspotifybackend.main import app
 
 
 def main() -> None:
     uvicorn.run(
-        "myspotifybackend.main:Application",
+        # "myspotifybackend.main:app",
+        "myspotifybackend.main:app",
         host="0.0.0.0",
         port=int(os.getenv("PORT", "")),
         reload=True,
-        factory=True,
     )
